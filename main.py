bcfba47edb2a102691570d30969f4f1ecbbecdc5
Austin Pickett <pickett.austin@gmail.com>
2022-02-06 16:14:18 -0500

    feat: add webbrowser open to steam url

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
-import random, requests, json, os
+import random, requests, json, os, webbrowser
 from dotenv import load_dotenv
 
+
 def random_game():
-  API_KEY = os.getenv('API_KEY')
-  STEAM_ID = os.getenv('STEAM_ID')
-  API_URL = f'https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={API_KEY}&include_appinfo=true&steamid={STEAM_ID}&format=json'
+    API_KEY = os.getenv("API_KEY")
+    STEAM_ID = os.getenv("STEAM_ID")
+    API_URL = f"https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={API_KEY}&include_appinfo=true&steamid={STEAM_ID}&format=json"
 
-  url = requests.get(API_URL)
-  data = json.loads(url.text)
+    url = requests.get(API_URL)
+    data = json.loads(url.text)
 
-  game_list = list(map(lambda game: game["name"], data["response"]["games"]))
+    game_list = list(
+        map(
+            lambda game: {"name": game["name"], "appid": game["appid"]},
+            data["response"]["games"],
+        )
+    )
 
-  print("You should play: " + random.choice(game_list))
+    game = random.choice(game_list)
 
-if __name__ == '__main__':
-  load_dotenv('.env')
-  random_game()
+    print(f"You should play: {game['name']}")
+    webbrowser.open(f"steam://store/{game['appid']}")
+
+
+if __name__ == "__main__":
+    load_dotenv(".env")
+    random_game()
